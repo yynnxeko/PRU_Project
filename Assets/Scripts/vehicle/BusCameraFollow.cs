192ccbc941d9291799b6389337962f64e6c3276a
binhvdn2004 <binhvu082004@gmail.com>
2026-01-18 15:05:46 +0700

    bus

---
+++ Assets/Scripts/vehicle/BusCameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class BusCameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothSpeed = 5f;
+
+    void LateUpdate()
+    {
+        if (target == null) return;
+
+        Vector3 desiredPos = new Vector3(
+            target.position.x,
+            target.position.y,
+            -10f
+        );
+
+        transform.position = Vector3.Lerp(
+            transform.position,
+            desiredPos,
+            smoothSpeed * Time.deltaTime
+        );
+    }
+}
