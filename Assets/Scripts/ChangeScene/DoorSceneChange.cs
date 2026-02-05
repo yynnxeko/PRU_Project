6f60d7379a4c06e67e39a672c991066f4ef66de0
NguyenGia-Phu <Phungse183613@fpt.edu.vn>
2026-02-05 21:37:15 +0700

    ChangeScenes

---
+++ Assets/Scripts/ChangeScene/DoorSceneChange.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoorSceneChange : MonoBehaviour
+{
+    public static string NextSpawnId;
+
+    [SerializeField] private string sceneToLoad;
+    [SerializeField] private string spawnIdInNextScene;
+    [SerializeField] private string playerTag = "Player";
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag(playerTag)) return;
+
+        NextSpawnId = spawnIdInNextScene;
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
